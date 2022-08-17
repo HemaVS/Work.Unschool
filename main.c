5f4ee2917a5e645a497ee2c8d77f83b0db86b29a
HemaVS <111344248+HemaVS@users.noreply.github.com>
2022-08-17 11:39:09 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ Code, Compile, Run and Debug online from anywhere in world.
  
 struct employees{
   char name[20];
-  int employeeid[1];
-  int experience[1];
-  int salary[1];
+  int employeeid;
+  int experience;
+  int salary;
   
 };
 int main()
@@ -21,21 +21,21 @@ int main()
     struct employees e[5];
     for(int i=0;i<5;i++){
         printf("Enter the name of employee");
-        scanf("%s", e[i].name);
+        scanf("%s", &e[i].name);
         printf("Enter the employee id ");
-        scanf("%d",e[i].employeeid);
+        scanf("%d",&e[i].employeeid);
         printf("Enter the years of experience");
-        scanf("%d",e[i].experience);
+        scanf("%d",&e[i].experience);
         printf("Enter salaray recieved");
-        scanf("%d",e[i].salary);
+        scanf("%d",&e[i].salary);
     }
 for(int i=0;i<5;i++)
 {
     printf("\n Now printing the details of employee %d",i+1);
     printf("\n Name of employee is %s", e[i].name);
-    printf("\n Employee id is %d",*e[i].employeeid);
-    printf("\n Years of experience is %d years",*e[i].experience);
-    printf("\n Salary is %d /- ",*e[i].salary);
+    printf("\n Employee id is %d",e[i].employeeid);
+    printf("\n Years of experience is %d years",e[i].experience);
+    printf("\n Salary is %d /- ",e[i].salary);
 }
 return 0;
 }
